b17c8845d399fb442d1d8922eb4c7be3603cb62c
j.nike <yannick.spriessler@gmail.com>
2023-10-09 19:25:25 +0200

    fn getUsername

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 //general------------------------------------------------------------------------
-console.log('Script loaded')
+console.log('Script.js loaded')
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('DOM Loaded');      //Prevent loading script before DOM is loaded
 })
 
+/* DEMO CODE
+TODO: @y-neck should be implemented correctly
+var userId;
+if(!userLoggedIn){
+    console.log('User could not be retrieved')
+} else{
+    userId = 'ID of logged-in user'
+}
+*/
 
 //index--------------------------------------------------------------------------
-
 //TODO: @joggiletti if user is not logged in, serve login site instead
 
 //login--------------------------------------------------------------------------
 
 //register-----------------------------------------------------------------------
 
-//profile/editProfile-------------------------------------------------------------
+//profile-------------------------------------------------------------------------
+//Get username of logged-in user
+async function getUsername(userId) {
+    const { data, error } = await supabase
+        .from('users')
+        .select('username')
+        .eq('id', userId)   //Get username where db id = userId 
+}
+
+
+//Get questpoints from database
+let questPoints;
+
+
+//editProfile---------------------------------------------------------------------
