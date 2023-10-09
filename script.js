0506678499b81b8a4b0f4d1686043859d1b29c79
j.nike <yannick.spriessler@gmail.com>
2023-10-09 19:41:42 +0200

    add general userId

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
 //import supabase
 import { supabase } from '../00_setup/supabase.js';
 
+var userId; //userID of logged-in user for further use
+
 /* DEMO CODE
 TODO: @y-neck should be implemented correctly
-var userId;
 if(!userLoggedIn){
     console.log('User could not be retrieved')
 } else{
