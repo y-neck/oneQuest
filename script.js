bb0d0eb728a6116949a0b611fa936c536da983f7
j.nike <yannick.spriessler@gmail.com>
2023-10-10 17:53:25 +0200

    refactoring fn getUsername

--- script.js
+++ script.js
@@ -105,33 +105,6 @@ imgElements.forEach((imgElement, index) => {
 //profile-------------------------------------------------------------------------
 var profileUsername;
 
-async function getUsername(userId) {
-    const { data, error } = await supa
-        .from('users')
-        .select('username')
-        .eq('id', userId)   //Get username where db id = userId 
-
-    if (error) {
-        console.error('Could not retrieve username from database')  //Add error handling
-    }
-    else {
-        profileUsername = data[0].username; //Assign username to profileUsername
-        $('#profile_username').innerHTML = profileUsername; //Replace default username with actual username
-    }
-    console.log('console.log in function ' + profileUsername);
-}
-//Test
-(async () => {
-    await getUsername(userId);
-    console.log('Async test: ' + profileUsername);
-})();
-
-//Trigger getUsername on pageload
-document.addEventListener('DOMContentLoaded', () => {
-    getUsername();
-})
-
-//Get questpoints from database
 
 
 //editProfile---------------------------------------------------------------------
