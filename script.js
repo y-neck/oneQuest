7bf96672b93025138c7a014574157412da4d9526
joggiletti <joelschaller2000@gmail.com>
2023-10-10 11:32:52 +0200

    upload img

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ imageFileInput.addEventListener('change', async (e) => {
 
     // Upload the image to the Supabase bucket
     const { data, error } = await supabase.storage
-      .from('YOUR_BUCKET_NAME') // Replace with your bucket name
+      .from('image_posts') // Replace with your bucket name
       .upload(filename, imageFile);
 
     if (error) {
