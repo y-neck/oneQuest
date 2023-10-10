93cf692ccb412d8dc1bbc7aa5001e772456d5212
joggiletti <joelschaller2000@gmail.com>
2023-10-10 13:17:29 +0200

    comments

--- 00_setup/supabase.js
+++ 00_setup/supabase.js
@@ -7,6 +7,6 @@ const supa = supabase.createClient(supabaseUrl, supabaseKey, {
     auth: {
         redirectTo: window.location.origin,  // This will redirect back to the page where the request originated from
     },
-});
+});        
 
 export { supa }
