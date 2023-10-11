a6a32f78cdd3d0d90bd176407f85cc37022cd2cd
j.nike <yannick.spriessler@gmail.com>
2023-10-11 16:08:20 +0200

    define input variables

--- 03_scripts/register.js
+++ 03_scripts/register.js
@@ -1,3 +1,9 @@
 //Import supabase
 import { supa } from '../00_setup/supabase.js';
 console.log('imported supabase');
+
+let registerEmail = $('#registerEmail');
+let registerUsername = $('#registerEmail');
+let registerPassword = $('#registerPassword');
+
+console.log(registerEmail)
