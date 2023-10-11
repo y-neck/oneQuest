eac6f44ec253ec540c76d6f9d8eac51b9df61ed8
j.nike <yannick.spriessler@gmail.com>
2023-10-11 14:18:00 +0200

    split js

--- mainUser.js
+++ mainUser.js
@@ -173,31 +173,6 @@ document.getElementById('daily_Quest_2').textContent = text;
 //register-----------------------------------------------------------------------
 
 //profile-------------------------------------------------------------------------
-var profileUsername;
-
-async function getUsername(userId) {
-    const { data, error } = await supa
-        .from('users')
-        .select('username')
-        .eq('id', userId);   //Get username where db id = userId
-    if (error) {
-        console.error('Could not retrieve username from database');  //Add error handling
-    }
-    else {
-        profileUsername = data[0].username; //Assign username to profileUsername
-        $('#profile_username').innerHTML = profileUsername; //Replace default username with actual username
-    }
-    console.log('console.log in function ' + profileUsername);
-}
-(async () => {
-    await getUsername(userId);
-    console.log('Async test: ' + profileUsername);
-})();
-
-//Trigger getUsername on pageload
-document.addEventListener('DOMContentLoaded', () => {
-    getUsername();
-});
 
 //editProfile---------------------------------------------------------------------
 
