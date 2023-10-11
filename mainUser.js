3070ff2452f0ea125734fbf2679fac1e5b1c47cc
j.nike <yannick.spriessler@gmail.com>
2023-10-11 15:46:48 +0200

    remove hard-coded userId

--- mainUser.js
+++ mainUser.js
@@ -1,7 +1,7 @@
 //import supabase
 import { supa } from './00_setup/supabase.js';
 
-var userId = 1; //userID of logged-in user for further use
+var userId; //userID of logged-in user for further use
 
 /* DEMO CODE
 TODO: @y-neck should be implemented correctly
