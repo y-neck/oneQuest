2b1dc7f74ab2f5bd223c3717a2540c426b280a4f
j.nike <yannick.spriessler@gmail.com>
2023-10-19 11:01:51 +0200

    fn loginUser

--- 03_scripts/login.js
+++ 03_scripts/login.js
@@ -1,6 +1,37 @@
 //import supabase
-import { supa } from './00_setup/supabase.js';
+import { supa } from '../00_setup/supabase.js';
 
 //import userId
 import { userId } from '../mainUser.js';
 
+//Login user
+async function loginUser() {
+    const loginEmail = document.querySelector('#loginEmail').value;
+    const loginPassword = document.querySelector('#loginPassword').value;
+
+    //Testing
+    //console.log(loginEmail,loginPassword)
+
+    /*     const { data, error } = await supa.auth.signInWithPassword({
+            username: loginEmail,
+            password: loginPassword,
+        }); */
+
+    const { error } = await supa.auth.signIn({
+        email: loginEmail,
+        password: loginPassword,
+    });
+    
+    if (error) {
+        console.error('Error logging in:', error.message);  //Add error handling
+    } else {
+        //Testing
+        //console.log('Logged in successfully as ', userId);
+        
+        //Redirect to index if login successful
+        window.location.href = "../index.html";
+    }
+}
+
+//Event listener for login button
+document.querySelector('#loginButton').addEventListener('click', loginUser);
