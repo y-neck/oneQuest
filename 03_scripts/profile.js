c7690e9bb263e97e418ad80ede81ee6f9e01b2b6
j.nike <yannick.spriessler@gmail.com>
2023-10-20 12:42:29 +0200

    serve login if user is not logged in

--- 03_scripts/profile.js
+++ 03_scripts/profile.js
@@ -5,6 +5,10 @@ import { supa } from '../00_setup/supabase.js';
 import { userId } from '../mainUser.js';
 console.log('Imported mainUser.js');
 
+//Serve login if user is not logged in
+if (userId === null) {
+    window.location.href = '../views/login.html';
+}
 
 //Replace default username with username from database
 async function getUsername(userId) {
