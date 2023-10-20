8b000cb768019d0a89e8df71d1158043119fe3f4
j.nike <yannick.spriessler@gmail.com>
2023-10-20 12:31:14 +0200

    comments

--- 03_scripts/profile.js
+++ 03_scripts/profile.js
@@ -8,42 +8,42 @@ console.log('Imported mainUser.js');
 
 //Replace default username with username from database
 async function getUsername(userId) {
-    const {data, error} = await supa
+    const { data, error } = await supa
         .from('users')
         .select('username')
         .eq('id', userId.id)   //Get username where db id = userId
         .single();
 
-/*     if (error) {
-        console.error('Could not retrieve username from database: ',error.message);  //Add error handling
-    } else { 
- */        document.querySelector('#profile_username').innerHTML = data.username; //Replace default username with actual username
-    //}
-    console.log('console.log in function ', data.username);
-}
+    if (error) {
+        console.error('Could not retrieve username from database: ', error.message);  //Add error handling
+    } else {
+        document.querySelector('#profile_username').innerHTML = data.username; //Replace default username with actual username
+        //}
+        console.log('console.log in function ', data.username);
+    }
 
-//Get questpoints from database
-async function getQuestpoints(userId) {
-    const { data, error } = await supa
-        .from('users')
-        .select('questScore')
-        .eq('id', userId);
+    //Get questpoints from database
+    async function getQuestpoints(userId) {
+        const { data, error } = await supa
+            .from('users')
+            .select('questScore')
+            .eq('id', userId.id);
 
-        if(data){
-            document.getElementById('profile_questpoints_number').innerHTML = data[0].questScore;
+        if (data) {
+            document.getElementById('profile_questpoints_number').innerHTML = data.questScore;
         } else {
             console.error('Could not retrieve questpoints from database');
         }
-}
-//Call functions
-(async () => {
-    await getUsername(userId);
-    await getQuestpoints(userId);
-})();
+    }
+    //Call functions
+    (async () => {
+        await getUsername(userId);
+        await getQuestpoints(userId);
+    })();
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('profile.js loaded')
-    console.log('DOM Loaded');      //Check if DOM is loaded
-    getUsername();
+    document.addEventListener('DOMContentLoaded', function () {
+        console.log('profile.js loaded')
+        console.log('DOM Loaded');      //Check if DOM is loaded
+        getUsername();
 
-});  //End of DOM loader
+    });  //End of DOM loader
