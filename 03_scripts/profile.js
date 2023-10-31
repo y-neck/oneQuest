e0f85f2847dcd364c723449ff191585b1a39f770
j.nike <yannick.spriessler@gmail.com>
2023-10-31 23:55:03 +0100

    add fn loadAvatar() to mainUser.js

--- 03_scripts/profile.js
+++ 03_scripts/profile.js
@@ -3,34 +3,12 @@ import { supa } from '../00_setup/supabase.js';
 
 //Import mainUser.js functions
 import { userId } from '../mainUser.js';
+import { loadAvatar } from '../mainUser.js';
 console.log('Imported mainUser.js');
 
 //Serve login if user is not logged in
 if (userId === null) {
     window.location.href = '../views/login.html';
-}
-
-//Load avatar----------------------------------------------------------------------------
-async function loadAvatar() {
-    const avatarPlaceholder = document.querySelector('.avatar');
-    //Fetch the avatar URL
-    const imageUrl = await supa
-        .from('users')
-        .select('avatar_url')
-        .eq('id', userId.id)
-        .single();
-    //Update avatar placeholder
-    if (imageUrl) {
-        //console.log('imageUrl:', imageUrl.data.avatar_url);
-        avatarPlaceholder.src = imageUrl.data.avatar_url;
-
-        // Set CSS properties to fit the image with correct aspect ratio
-        avatarPlaceholder.style.width = '100%';
-        avatarPlaceholder.style.height = '100%';
-        avatarPlaceholder.style.objectFit = 'cover';
-    } else {
-        console.error('Error fetching avatar URL:', error.message);
-    }
 }
 
 //Replace default username with username from database-----------------------------------
--- mainUser.js
+++ mainUser.js
@@ -5,4 +5,29 @@ import { supa } from './00_setup/supabase.js';
 const userId = await supa.auth.user(); //userId of logged-in user for further use
 console.log('userId:', userId);
 
+//Load avatar----------------------------------------------------------------------------
+async function loadAvatar() {
+    const avatarPlaceholder = document.querySelector('.avatar');
+    //Fetch the avatar URL
+    const imageUrl = await supa
+        .from('users')
+        .select('avatar_url')
+        .eq('id', userId.id)
+        .single();
+    //Update avatar placeholder
+    if (imageUrl) {
+        //console.log('imageUrl:', imageUrl.data.avatar_url);
+        avatarPlaceholder.src = imageUrl.data.avatar_url;
+
+        // Set CSS properties to fit the image with correct aspect ratio
+        avatarPlaceholder.style.width = '100%';
+        avatarPlaceholder.style.height = '100%';
+        avatarPlaceholder.style.objectFit = 'cover';
+    } else {
+        console.error('Error fetching avatar URL:', error.message);
+    }
+}
+
+//Exports
 export { userId };
+export { loadAvatar };
