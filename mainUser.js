b638ee068846d7faa08ad32d450ab073a5f03b5c
j.nike <yannick.spriessler@gmail.com>
2023-11-01 11:41:21 +0100

    add avatar to headers

--- mainUser.js
+++ mainUser.js
@@ -7,26 +7,30 @@ console.log('userId:', userId);
 
 //Load avatar----------------------------------------------------------------------------
 async function loadAvatar() {
-    const avatarPlaceholder = document.querySelector('.avatar');
-    //Fetch the avatar URL
-    const imageUrl = await supa
-        .from('users')
-        .select('avatar_url')
-        .eq('id', userId.id)
-        .single();
-    //Update avatar placeholder
-    if (imageUrl) {
-        //console.log('imageUrl:', imageUrl.data.avatar_url);
-        avatarPlaceholder.src = imageUrl.data.avatar_url;
+    try {
+        const avatarPlaceholder = document.querySelector('.avatar');
+        //Fetch the avatar URL
+        const imageUrl = await supa
+            .from('users')
+            .select('avatar_url')
+            .eq('id', userId.id)
+            .single();
+        //Update avatar placeholder
+        if (imageUrl) {
+            //console.log('imageUrl:', imageUrl.data.avatar_url);
+            avatarPlaceholder.src = imageUrl.data.avatar_url;
 
-        // Set CSS properties to fit the image with correct aspect ratio
-        avatarPlaceholder.style.width = '100%';
-        avatarPlaceholder.style.height = '100%';
-        avatarPlaceholder.style.objectFit = 'cover';
-    } else {
+            // Set CSS properties to fit the image with correct aspect ratio
+            avatarPlaceholder.style.objectFit = 'cover';
+        } else {
+            console.error('Error fetching avatar URL:', error.message);
+        }
+    } catch (error) {
         console.error('Error fetching avatar URL:', error.message);
     }
 }
+
+document.addEventListener('DOMContentLoaded', loadAvatar);
 
 //Exports
 export { userId };
